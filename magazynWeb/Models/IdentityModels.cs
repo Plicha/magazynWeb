9e138930d95f8a75352203a1cd4e8b7d42ba6b77
Jan Michalski <janol95@gmail.com>
2018-01-08 14:38:47 +0100

    Dodany model i baza danych

--- magazynWeb/Models/IdentityModels.cs
+++ magazynWeb/Models/IdentityModels.cs
@@ -20,6 +20,8 @@ namespace magazynWeb.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Item> Items { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
---
+++ magazynWeb/Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace magazynWeb.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public int Year { get; set; }
+        public int Month { get; set; }
+    }
+}
