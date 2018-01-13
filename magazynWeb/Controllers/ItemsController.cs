870581df3ed232da7515a7f965e63cdc64fc1c10
Jan Michalski <janol95@gmail.com>
2018-01-13 20:23:54 +0100

    baza v2

---
+++ magazynWeb/Controllers/ItemsController.cs
@@ -0,0 +1,19 @@
+﻿using magazynWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace magazynWeb.Controllers
+{
+    public class ItemsController : Controller
+    {
+        // GET: Items
+        public ActionResult Magazyny()
+        {
+            var magazyn = new Item();
+            return View();
+        }
+    }
+}
